f0480d44d051ba8500acc3d2ea195ea41923a6c8
Wilberk <ledezma.wilberk@gmail.com>
2024-09-28 15:45:48 -0400

    added all certifications

--- src/utils/sortDate.ts
+++ src/utils/sortDate.ts
@@ -1,6 +1,6 @@
 export default function sortDate(dateArray: Array<{ year: number, name: string, date: string, url: string, icon: any }>) {
   return dateArray.sort((a, b) => {
-    if (a.year < b.year) {
+    if (a.year > b.year) {
       return -1
     }
 
